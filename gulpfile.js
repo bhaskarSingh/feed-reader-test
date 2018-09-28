8a2d6138f44a25c8d3c18737b6fe6e36d41b3e0c
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-09-29 02:22:10 +0530

    feat: make js code backward compatible and compress

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 function copy() {
     return gulp.src([
@@ -23,4 +25,28 @@ function serve() {
     });
 }
 
-gulp.task('buildAndServe', gulp.series(copy, serve));
+function processJs() {
+    return gulp.src(['app/js/*.js'])
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(['build/js']));
+}
+
+function processSpec() {
+    return gulp.src(['app/jasmine/spec/*.js'])
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(['build/jasmine/spec']));
+}
+
+gulp.task(
+    'buildAndServe',
+    gulp.series(
+        copy,processJs, processSpec,
+        serve
+    )
+);
